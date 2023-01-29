4c5dac77bf1e9516b1f826130db6f753c1939044
gkavin57 <gkavin77@gmail.com>
2023-01-29 17:15:14 +0530

    user

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDb = require("./config/connectDb");
+// config dot env file
+dotenv.config();
+
+//databse call
+connectDb();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use("/users", require("./routes/userRoute"));
+app.get("/", (req, res) => {
+  res.send("this is expense server");
+});
+
+//por
+const PORT = 8080 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
